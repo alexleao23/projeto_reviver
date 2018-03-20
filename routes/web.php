69063df54acaf45365306eeed5b62073c16e2346
alexleao23 <alexandersilvadeleao23@gmail.com>
2018-03-20 16:09:33 -0300

    Modificações nas rotas

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,13 @@ Route::get('/', function () {
     return redirect('login');
 });
 
-Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{id}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 	Route::get('/', function(){
@@ -13,6 +19,6 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 	Route::resource('pacientes', 'PacientesController');
 	Route::resource('questionarios_nutricao', 'QuestionariosNutricaoController');
 	Route::resource('responsaveis', 'ResponsaveisController');
-	Route::get('register', 'Auth\RegisterController@showRegistrationForm')->middleware('can:create,App\User');
+	Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register')->middleware('can:create,App\User');
 	Route::post('register', 'Auth\RegisterController@register')->middleware('can:create,App\User');
 });
