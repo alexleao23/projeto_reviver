a9a69311a99def1e4785008f1b09169ca6ebcba9
João Victor Machado de Andrade <joaovictormachadodeandrade@outlook.com>
2018-03-23 15:12:01 -0300

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
               </span>
           @endif
         </div>
-        {{-- <div class="form-group has-feedback">
+        <div class="form-group has-feedback">
           <input name="email" type="email" class="form-control" placeholder="E-mail" {{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') }}" required>
           <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
           @if ($errors->has('email'))
@@ -29,8 +29,8 @@
                   <strong>{{ $errors->first('email') }}</strong>
               </span>
           @endif
-        </div> --}}
-        {{-- <div class="form-group has-feedback">
+        </div>
+        <div class="form-group has-feedback">
           <input name="cpf" class="form-control cpf" placeholder="CPF" {{ $errors->has('cpf') ? ' is-invalid' : '' }}" value="{{ old('cpf') }}" required>
           <span class="glyphicon glyphicon-user form-control-feedback"></span>
           @if ($errors->has('cpf'))
@@ -38,7 +38,7 @@
                   <strong>{{ $errors->first('cpf') }}</strong>
               </span>
           @endif
-        </div> --}}
+        </div>
         <div class="form-group has-feedback">
           <input name="celular" type="celular" class="form-control telefone" placeholder="Celular(Ex: 99999-9999)" {{ $errors->has('celular') ? ' is-invalid' : '' }}" value="{{ old('celular') }}" required>
           <span class="glyphicon glyphicon-earphone form-control-feedback"></span>
