7dc42d99d0a2ffc8fb3eb77c2814d6d1491d2932
alexleao23 <alexandersilvadeleao23@gmail.com>
2018-04-11 16:15:30 -0300

    Tentativa

--- resources/views/form_build/radio2.blade.php
+++ resources/views/form_build/radio2.blade.php
@@ -2,7 +2,7 @@
 	$classdiv = $classdiv ?? 'col-md-4';
 	$class = $class ?? 'flat-red';
 @endphp
-<div class={{ $classdiv }}>
+<div class="{{ $classdiv }}">
 	<div class="form-group">
 		{{ Form::label($nome, $label) }}<br>
 		@for ($i = 0; $i < count($opcoes); $i++)
