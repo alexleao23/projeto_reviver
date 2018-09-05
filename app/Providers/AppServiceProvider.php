79507dba7a51cc6ef3df1298387eb92895720ad7
joaomchd <joaovictormachadodeandrade@outlook.com>
2018-09-05 18:30:07 -0300

    prolema mysql resolvido

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,10 +13,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-    }
+public function boot()
+{
+    Schema::defaultStringLength(191);
+}
 
     /**
      * Register any application services.
