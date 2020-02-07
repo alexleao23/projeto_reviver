5b61de887cb577828b866b44f7d73faf4e569d55
Alexander Silva de Leão <alexandersilvadeleao23@gmail.com>
2020-02-07 11:34:03 -0300

    Ajuste no seeder para adicionar primeiros usuários ao sistema

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(ResponsaveisTableSeeder::class);
-        $this->call(PacientesTableSeeder::class);
+        // $this->call(ResponsaveisTableSeeder::class);
+        // $this->call(PacientesTableSeeder::class);
     }
 }
